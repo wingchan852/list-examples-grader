6d615facfe872a9313ca946ea9b9ec5c20e4b713
Wing <chanwingyin10@gmail.com>
2022-11-10 23:16:02 -0800

    first commit

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,52 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+class SomeStringChecker implements StringChecker {
+
+  @Override
+  public boolean checkString(String s) {
+    if (s.indexOf('a') == 0 || s.indexOf('e') == 0 || s.indexOf('i') == 0 || s.indexOf('o') == 0
+        || s.indexOf('u') == 0)
+      return true;
+    return false;
+  }
+
+}
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFilter() {
+    List<String> input = Arrays.asList("apple", "banana");
+    List<String> output = Arrays.asList("apple");
+    SomeStringChecker stringChecker = new SomeStringChecker();
+    assertEquals(output, ListExamples.filter(input, stringChecker));
+  }
+
+  @Test
+  public void testFilter2() {
+    List<String> input = Arrays.asList("banana");
+    List<String> output = Arrays.asList("");
+    SomeStringChecker stringChecker = new SomeStringChecker();
+    assertEquals(output, ListExamples.filter(input, stringChecker));
+  }
+
+  @Test
+  public void testFilter3() {
+    List<String> input = Arrays.asList("apple", "apple", "banana");
+    List<String> output = Arrays.asList("apple", "apple");
+    SomeStringChecker stringChecker = new SomeStringChecker();
+    assertEquals(output, ListExamples.filter(input, stringChecker));
+  }
+
+  @Test
+  public void testMerge3() {
+    List<String> list1 = Arrays.asList("apple", "cat", "banana");
+    List<String> list2 = Arrays.asList("banana");
+    List<String> output = Arrays.asList("apple", "banana", "banana", "cat");
+    assertEquals(output, ListExamples.merge(list1, list2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,30 @@
 # Create your grading script here
-
 set -e
-
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 rm -rf student-submission
 git clone $1 student-submission
 
+# check file exist
+if [[ -f "./student-submission/ListExamples.java" ]]
+then 
+    echo "correct file submitted"
+else 
+    echo "missing file"
+fi
+
+# copy the test & lib to student-submission
+cp TestListExamples.java ./student-submission/
+cp -r lib ./student-submission/
+set +e
+# compile the test & student code
+cd student-submission
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 1 > stdout 2 > stderr
+
+
+if [[ $? -eq 0 ]]
+then 
+    echo "all tests passed!"
+else    
+    echo "test filed!"
+fi
